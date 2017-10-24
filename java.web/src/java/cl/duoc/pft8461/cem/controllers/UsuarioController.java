08e022849ee3d0fc0e2a94a30845bb27bf9564cc
Joe <Jos@Joe>
2017-10-23 21:34:42 -0300

    Revert "- Correcciones menores de visualización - Corrección de links perfil y desloguear"
    This reverts commit 52a27c00b99eeed2a414c2e8ea5b508b46bb220e.

--- java.web/src/java/cl/duoc/pft8461/cem/controllers/UsuarioController.java
+++ java.web/src/java/cl/duoc/pft8461/cem/controllers/UsuarioController.java
@@ -24,7 +24,6 @@ import cl.duoc.pft8461.cem.ws.MenuWS_Service;
 import cl.duoc.pft8461.cem.ws.PerfilUsuario;
 import cl.duoc.pft8461.cem.ws.PerfilUsuarioWS_Service;
 import cl.duoc.pft8461.cem.ws.Usuario;
-import cl.duoc.pft8461.cem.ws.UsuarioWS;
 import cl.duoc.pft8461.cem.ws.UsuarioWS_Service;
 import java.util.LinkedList;
 import sun.reflect.generics.tree.Tree;
@@ -33,123 +32,55 @@ import sun.reflect.generics.tree.Tree;
  *
  * @author Joe
  */
+
 @Controller
 @SessionAttributes
 public class UsuarioController {
-
-    private final UsuarioWS usuarioWS = new UsuarioWS_Service().getUsuarioWSPort();
-    private List<PerfilUsuario> perfilesUsuario;
-
+    
+    private final UsuarioWS_Service ws = new UsuarioWS_Service();
+    private  List<PerfilUsuario> perfilesUsuario;
+    
     public UsuarioController() {
         PerfilUsuarioWS_Service perfilUsuarioWS = new PerfilUsuarioWS_Service();
         perfilesUsuario = perfilUsuarioWS.getPerfilUsuarioWSPort().findAllPerfilUsuario();
     }
-
+    
     /**
-     * Método form, que carga el formulario de login de la aplicación del
-     * Sistema CEM.
-     *
+     * Método form, que carga el formulario de login
+     * de la aplicación del Sistema CEM.
+     * 
      * @param request
      * @param response
      * @return
      * @throws ServletException
-     * @throws IOException
+     * @throws IOException 
      */
     @RequestMapping(value = {"usuario/lista.htm"}, method = RequestMethod.GET)
-    public ModelAndView lista(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    public ModelAndView form(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
         ModelAndView mav = new ModelAndView();
-
-        List<Usuario> listaUsuario = usuarioWS.findAllUsuarios();
+        UsuarioWS_Service usuarioWS = new UsuarioWS_Service();
+        List<Usuario> listaUsuario = usuarioWS.getUsuarioWSPort().findAllUsuarios();
         mav.addObject("listado", listaUsuario);
-        mav.addObject("perfilesUsuario", perfilesUsuario);
-
+        mav.addObject("perfilesUsuario",perfilesUsuario);
+        
         mav.addObject("listaMenu", this.getMenu());
         mav.addObject("tituloPagina", "Usuario");
         mav.addObject("subtituloPagina", "Listado de Usuarios:");
         mav.setViewName("usuarioLista");
         return mav;
     }
-
-    @RequestMapping(value = {"usuario/nuevo.htm"}, method = RequestMethod.GET)
-    public ModelAndView nuevo(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        ModelAndView mav = new ModelAndView();
-
-        mav.addObject("perfilesUsuario", perfilesUsuario);
-        mav.setViewName("usuarioForm");
-
-        return mav;
-    }
-
-    @RequestMapping(value = {"usuario/editar.htm"}, method = RequestMethod.GET)
-    public ModelAndView editar(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        ModelAndView mav = new ModelAndView();
-
-        Usuario usr = usuarioWS.findUsuario(Integer.parseInt(request.getParameter("id")));
-        mav.addObject("usr", usr);
-        mav.addObject("perfilesUsuario", perfilesUsuario);
-        mav.setViewName("usuarioForm");
-
-        return mav;
-    }
-
-    @RequestMapping(value = {"usuario/borrar.htm"}, method = RequestMethod.GET)
-    public ModelAndView borrarGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        ModelAndView mav = new ModelAndView();
-        Usuario usr = usuarioWS.findUsuario(Integer.parseInt(request.getParameter("id")));
-        mav.addObject("usr", usr);
-        mav.setViewName("usuarioBorrar");
-        return mav;
-    }
-
-    @RequestMapping(value = {"usuario/borrar.htm"}, method = RequestMethod.POST)
-    public void borrarPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        ModelAndView mav = new ModelAndView();
-        usuarioWS.removeUsuario(Integer.parseInt(request.getParameter("inputIdUsuario")));
-
-        response.sendRedirect("lista.htm");;
-    }
-
-    @RequestMapping(value = {"usuario/guardar.htm"}, method = RequestMethod.POST)
-    public void guardar(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        if (request.getParameter("inputIdUsuario") == null) {
-            usuarioWS.createUsuario(
-                    request.getParameter("inputUsuario"),
-                    request.getParameter("inputClave"),
-                    request.getParameter("inputNombre"),
-                    request.getParameter("inputApellidoPat"),
-                    request.getParameter("inputApellidoMat"),
-                    request.getParameter("inputEmail"),
-                    Integer.parseInt(request.getParameter("inputPerfilUsuario")));
-        } else {
-            usuarioWS.editUsuario(
-                    Integer.parseInt(request.getParameter("inputIdUsuario")),
-                    request.getParameter("inputUsuario"),
-                    request.getParameter("inputNombre"),
-                    request.getParameter("inputApellidoPat"),
-                    request.getParameter("inputApellidoMat"),
-                    request.getParameter("inputEmail"),
-                    Integer.parseInt(request.getParameter("inputPerfilUsuario")));
-        }
-        response.sendRedirect("lista.htm");
-
-    }
-
-    private List<ArbolMenu> getMenu() {
+    
+    private List<ArbolMenu> getMenu(){
         MenuWS_Service menuWS = new MenuWS_Service();
         MenuItemWS_Service menuItemWS = new MenuItemWS_Service();
         List<ArbolMenu> arbol = new LinkedList<>();
         List<Menu> listaMenu = menuWS.getMenuWSPort().findMenuPor("PERFIL_USUARIO", "1");
-
-        for (Menu m : listaMenu) {
-            ArbolMenu am = new ArbolMenu(m);
-            am.setListaMenu(menuItemWS.getMenuItemWSPort().findMenuItemPor("ID_MENU", m.getIdMenu().toString()));
-            arbol.add(am);
+        
+        for (Menu m : listaMenu){
+           ArbolMenu am = new ArbolMenu(m);
+           am.setListaMenu(menuItemWS.getMenuItemWSPort().findMenuItemPor("ID_MENU", m.getIdMenu().toString()));
+           arbol.add(am);
         }
         return arbol;
     }
--- java.web/web/WEB-INF/jsp/include/sidebar.jsp
+++ java.web/web/WEB-INF/jsp/include/sidebar.jsp
@@ -1,42 +1,3 @@
-<<<<<<< HEAD
-<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<aside class="main-sidebar">
-    <!-- sidebar: style can be found in sidebar.less -->
-    <section class="sidebar">
-<c:forEach items="${listaMenu}" var="menu">
-    ${menu.getTitulo()}<br>
-</c:forEach>
-      <!-- sidebar menu: : style can be found in sidebar.less -->
-      <ul class="sidebar-menu" data-widget="tree">
-          
-    <c:forEach items="${listaMenu}" var="menu">
-        <li class="header">${menu.getTitulo().toUpperCase()}</li>
-        
-            <c:forEach items="${menu.getListaMenu()}" var="menuItem">
-                <c:if test="${menuItem.getPadre()==0}">
-                    <li class="treeview">
-                        <a href="${menuItem.getUrl()}">
-                            <i class="fa ${menuItem.getIcono()}"></i>
-                            <span>${menuItem.getTitulo()}</span>
-                            <span class="pull-right-container">
-                                <i class="fa fa-angle-left pull-right"></i>
-                            </span>
-                            <ul class="treeview-menu">
-                                <c:forEach items="${menu.getListaMenu()}" var="menuItem2">
-                                  <c:if test="${menuItem2.getPadre()==menuItem.getIdMenuItem()}">
-                                     <li><a href="${menuItem2.getUrl()}"><i class="fa fa-circle-o"></i> ${menuItem2.getTitulo()}</a></li>
-                                  </c:if>
-                                </c:forEach>
-                            </ul>
-                        </a>
-                    </li>
-                </c:if> 
-            </c:forEach>
-    </c:forEach>
-    </section>
-    <!-- /.sidebar -->
-  </aside>
-=======
 <%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <aside class="main-sidebar">
     <!-- sidebar: style can be found in sidebar.less -->
@@ -72,4 +33,3 @@
     </section>
     <!-- /.sidebar -->
   </aside>
->>>>>>> 280b4781a8b2121fff006f96a3bfca0729bf4288
